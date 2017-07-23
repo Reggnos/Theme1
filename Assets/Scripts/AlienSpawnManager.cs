892b68eea0aa55244fe628a46a43b2340419b10d
Mateja Stankovic <stankovicmateja@yahoo.com>
2017-07-23 18:13:53 +0200

    Alien, Mix Table, Character and enemy spawning

---
+++ Assets/Scripts/AlienSpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienSpawnManager : MonoBehaviour {
+
+    public GameObject[] enemies;
+    private GameObject enemyToSpawn;
+    public GameObject pos1;
+    public GameObject pos2;
+    private Vector3 randomX;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("SpawnEnemy" , 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnEnemy()
+    {
+        randomX = new Vector3(Random.Range(pos1.transform.position.x, pos2.transform.position.x), transform.position.y);
+        enemyToSpawn = enemies[0];
+        Instantiate(enemyToSpawn);
+        enemyToSpawn.transform.position = randomX;
+        Invoke("SpawnEnemy", 1);
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public int health = 10;
+    public int score;
+
+
+	// Use this for initialization
+	void Start () {
+        health = 10;
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
