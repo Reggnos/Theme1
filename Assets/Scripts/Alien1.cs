ef7be21b44b5f333da7bae3ad59bafa9e401fbc9
Mateja Stankovic <stankovicmateja@yahoo.com>
2017-07-24 12:34:13 +0200

    Popravljen bug

--- Assets/Scripts/Alien1.cs
+++ Assets/Scripts/Alien1.cs
@@ -9,26 +9,13 @@ public class Alien1 : MonoBehaviour {
     public GameObject otherGameObject;
     private Transform goal;
 
-<<<<<<< HEAD
-	// Use this for initialization
-	void Start ()
-    {
-=======
 	
 	void Start () {
->>>>>>> db8002ca4e675fa227c3903b4263cfef6350baf4
         goal = otherGameObject.GetComponent<Transform>();
         characterScript = character.GetComponent<Character>();
 	}
-	    
-<<<<<<< HEAD
-	// Update is called once per frame
-	void Update ()
-    {
-=======
-	
+
 	void Update () {
->>>>>>> db8002ca4e675fa227c3903b4263cfef6350baf4
         transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, goal.transform.position.y), 3 * Time.deltaTime);
     }
 
